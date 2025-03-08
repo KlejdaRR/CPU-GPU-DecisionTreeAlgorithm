682047001a93e4a42973c6afce9bf1d0376eb7df
Klejda Rrapaj <rrapaj41@gmail.com>
2025-03-08 17:24:50 +0100

    addition of comments

--- data_loader.h
+++ data_loader.h
@@ -4,6 +4,10 @@
 #include <vector>
 #include <string>
 
+/**
+ * This class provides a method to load the Iris dataset from a CSV file.
+ */
+
 class DataLoader {
 public:
     void load_iris_dataset(const std::string& filepath, std::vector<std::vector<double>>& data, std::vector<int>& labels);
